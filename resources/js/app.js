7e214257824f613184f5bf9ffe82e319f3444333
Telemachus19 <aashrafmohamed84@gmail.com>
2026-05-02 16:12:26 +0300

    feat: integrate Inertia.js with React for the Home page (empty white page at the momnt :-)
    - Updated the web routes to render the Home page using Inertia.
    - Configured Vite to support React and updated the input files.
    - Created a new Home component in React with a layout for the landing page.
    - Set up the main app entry point for Inertia with React.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-//
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+const appName = document.querySelector("title")?.innerText || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx"),
+        ),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+    progress: {
+        color: "#4b5563",
+    },
+});
