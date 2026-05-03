48666a02464a19ec7bba25859f97ca1268985f7c
Telemachus19 <aashrafmohamed84@gmail.com>
2026-05-03 10:56:56 +0300

    feat: add Events for Order for instant updates

---
+++ app/Events/OrderStatusUpdated.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderStatusUpdated implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $orderId;
+    public $status;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(int $orderId, string $status)
+    {
+        $this->orderId = $orderId;
+        $this->status = $status;
+    }
+
+    /**
+     * The event's broadcast name.
+     */
+    public function broadcastAs(): string
+    {
+        return 'order.status.updated';
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('admin.orders'),
+        ];
+    }
+}
